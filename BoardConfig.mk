d8af4cbc21f2a29b439f907b9c1a1378e3e82eee
marc0601 <marckhalil06@gmail.com>
2018-09-12 16:36:06 +0200

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,68 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/t03g
+
+TARGET_BOARD_PLATFORM := exynos4
+TARGET_BOOTLOADER_BOARD_NAME := t03g
+
+# Recovery
+BOARD_NAND_PAGE_SIZE := 4096
+BOARD_NAND_SPARE_SIZE := 128
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12381585408
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_PARTITION_SIZE := 1048576
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+TARGET_USERIMAGES_USE_F2FS := true
+TW_THEME := portrait_mdpi
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_NO_REBOOT_BOOTLOADER := true
+HAVE_SELINUX := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_INCLUDE_CRYPTO := true
+TW_EXCLUDE_SUPERSU := true
+LZMA_RAMDISK_TARGETS := boot,recovery 
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRX_8888"
+TW_NO_USB_STORAGE := true
+TWRP_EVENT_LOGGING := false
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_USE_SAMSUNG_COLORFORMAT := true
+BOARD_NONBLOCK_MODE_PROCESS := true
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USES_MFC_FPS := true
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_CSC_FIMC := false
+
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig32.mk
